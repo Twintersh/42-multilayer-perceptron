21d49269e3f1d8521bec4f5e628cffab060246aa
Tom Wintersheim <twinters@car5s3.42barcelona.com>
2024-11-20 13:10:57 +0100

    Format

--- separateDataset.py
+++ separateDataset.py
@@ -5,15 +5,15 @@ a = 0.90 # separator factor
 
 def linecount(file_path):
 	with open(file_path, 'r') as f:
-		return sum(1 for line in f)
+		return sum(1 for _ in f)
 
 if __name__ == "__main__":
 	datasets_dir = "datasets"
 	os.makedirs("datasets", exist_ok=True)
 
-	source_file =	"data.csv"
-	train_file =	os.path.join(datasets_dir, "train.csv")
-	predict_file =	os.path.join(datasets_dir, "predict.csv")
+	source_file		= "data.csv"
+	train_file		= os.path.join(datasets_dir, "train.csv")
+	predict_file	= os.path.join(datasets_dir, "predict.csv")
 
 	with open(source_file, 'r') as data, open(train_file, 'w') as train, open(predict_file, 'w') as predict:
 		for line in data:
