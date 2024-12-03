94bde182d7aed981ce9bfd4ec12d43f961fc0d30
Twintersh <tom.wintersheim@gmail.com>
2024-12-03 15:55:35 +0100

    Deleted choice choose splited dataset path

--- splitDataset.py
+++ splitDataset.py
@@ -4,25 +4,20 @@ import argparse
 import shelve
 import sys
 
-a = 0.90 # default split
 parser = argparse.ArgumentParser(description="A program that splits a DataSet")
-parser.add_argument("-p", "--path", type=str, help="The location you want to store splited database")
 parser.add_argument("-a", "--split_factor", type=float, help="split factor (default value 0.9)")
 
 def linecount(file_path):
 	with open(file_path, 'r') as f:
 		return sum(1 for _ in f)
 
-if __name__ == "__main__":
+def main():
+	a = 0.50 # default split
+	datasets_dir = "datasets"
 	args = parser.parse_args()
-	if len(sys.argv) == 1:
-		parser.print_usage()
-		sys.exit()
-	datasets_dir = args.path
-	if args.split_factor  is not None:
-		a = args.split_factor
-	file = shelve.open(".save_parameters")
-	file["datasets_dir"] = datasets_dir
+	with shelve.open(".save_parameters") as file:
+		if args.split_factor is not None:
+			a = args.split_factor
 	os.makedirs(datasets_dir, exist_ok=True)
 
 	source_file		= "data.csv"
@@ -39,3 +34,6 @@ if __name__ == "__main__":
 	print(f"length train file:\t{linecount(train_file)}")
 	print(f"length predict file:\t{linecount(predict_file)}")
 	print(f"factor of separation :\t{a}")
+
+if __name__ == "__main__":
+	main()
--- train.py
+++ train.py
@@ -8,72 +8,72 @@ import shelve
 from copy import deepcopy
 
 def train():
-	save_file = shelve.open(".save_parameters")
-	(train_data,
-	train_label,
-	pred_data,
-	pred_label) 		= getDataFromDataset(save_file["datasets_dir"])
-	hidden_layer_size 	= 30
-	l_rate 				= 1e-3
-	batch_size			= 100
-	iterate				= 2500 # number of times we go through the dataset
-	epochs				= int(len(train_data) / batch_size) + 1
-	loss_history = []
-	loss_pred_history = []
-	epochs_history = []
+	with shelve.open(".save_parameters") as save_file:
+		(train_data,
+		train_label,
+		pred_data,
+		pred_label) 		= getDataFromDataset("datasets")
+		hidden_layer_size 	= 30
+		l_rate 				= 1e-3
+		batch_size			= 100
+		iterate				= 2500 # number of times we go through the dataset
+		epochs				= int(len(train_data) / batch_size) + 1
+		loss_history = []
+		loss_pred_history = []
+		epochs_history = []
 
-	print(f"training data shape :\t{train_data.shape}")
-	print(f"prediction data shape :\t{pred_data.shape}")
+		print(f"training data shape :\t{train_data.shape}")
+		print(f"prediction data shape :\t{pred_data.shape}")
 
-	# setting the layers
-	layers = [
-		Layer(Sigmoid, hidden_layer_size, l_rate),
-		Layer(Sigmoid, hidden_layer_size, l_rate),
-		Layer(Sigmoid, hidden_layer_size, l_rate),
-		Layer(Softmax, 2, l_rate),
-	]
-	loss_layer = BinaryCrossEntropy()
+		# setting the layers
+		layers = [
+			Layer(Sigmoid, hidden_layer_size, l_rate),
+			Layer(Sigmoid, hidden_layer_size, l_rate),
+			Layer(Sigmoid, hidden_layer_size, l_rate),
+			Layer(Softmax, 2, l_rate),
+		]
+		loss_layer = BinaryCrossEntropy()
 
-	# init the MLP with the appropriate layers
-	mlp = MultilayerPerceptron(layers, loss_layer, batch_size)
+		# init the MLP with the appropriate layers
+		mlp = MultilayerPerceptron(layers, loss_layer, batch_size)
 
-	nb_epoch = 0
-	for i in range(iterate * epochs):
-		# creating batches
-		if (batch_size > len(train_data)):
-			rand_index = range(len(train_data))
-		else:
-			rand_index = random.sample(range(len(train_data)), batch_size)
-		batch_data = np.array([train_data[j] for j in rand_index])
-		batch_label = np.array([train_label[j] for j in rand_index])
+		nb_epoch = 0
+		for i in range(iterate * epochs):
+			# creating batches
+			if (batch_size > len(train_data)):
+				rand_index = range(len(train_data))
+			else:
+				rand_index = random.sample(range(len(train_data)), batch_size)
+			batch_data = np.array([train_data[j] for j in rand_index])
+			batch_label = np.array([train_label[j] for j in rand_index])
 
-		if (batch_size > len(pred_data)):
-			rand_index = range(len(pred_data))
-		else:
-			rand_index = random.sample(range(len(pred_data)), batch_size)
-		batch_pred = np.array([pred_data[j] for j in rand_index])
-		batch_pred_label = np.array([pred_label[j] for j in rand_index])
+			if (batch_size > len(pred_data)):
+				rand_index = range(len(pred_data))
+			else:
+				rand_index = random.sample(range(len(pred_data)), batch_size)
+			batch_pred = np.array([pred_data[j] for j in rand_index])
+			batch_pred_label = np.array([pred_label[j] for j in rand_index])
 
-		# Learning 🧠
-		loss = mlp.calculate_loss(batch_data, batch_label)
-		loss_pred = deepcopy(mlp).calculate_loss(batch_pred, batch_pred_label)
-		mlp.backward(batch_label)
-		# each time i go through the entire dataset (approximation), print loss
-		if not i % epochs or i == (iterate * epochs) - 1:
-			if not i % (epochs * 100) or i == (iterate * epochs) - 1:
-				print(f"epoch : {nb_epoch}/{iterate}\tloss: {loss:.4f}\tval_loss: {loss_pred:.4f}")
-			nb_epoch += 1
-			loss_history.append(loss)
-			loss_pred_history.append(loss_pred)
-			epochs_history.append(i / epochs)
+			# Learning 🧠
+			loss = mlp.calculate_loss(batch_data, batch_label)
+			loss_pred = deepcopy(mlp).calculate_loss(batch_pred, batch_pred_label)
+			mlp.backward(batch_label)
+			# each time i go through the entire dataset (approximation), print loss
+			if not i % epochs or i == (iterate * epochs) - 1:
+				if not i % (epochs * 100) or i == (iterate * epochs) - 1:
+					print(f"epoch : {nb_epoch}/{iterate}\tloss: {loss:.4f}\tval_loss: {loss_pred:.4f}")
+				nb_epoch += 1
+				loss_history.append(loss)
+				loss_pred_history.append(loss_pred)
+				epochs_history.append(i / epochs)
 
-	save_file["network"] = mlp
-	plt.plot(epochs_history, loss_history, color='red')
-	plt.plot(epochs_history, loss_pred_history, color='b')
-	plt.title("loss function by epoch")
-	plt.xlabel('epochs')
-	plt.ylabel('Loss')
-	plt.show()
+		save_file["network"] = mlp
+		plt.plot(epochs_history, loss_history, color='red')
+		plt.plot(epochs_history, loss_pred_history, color='b')
+		plt.title("loss function by epoch")
+		plt.xlabel('epochs')
+		plt.ylabel('Loss')
+		plt.show()
 
 
 # call main
