1ff2819fe4cc1a2f13b844462228ef8ba78890d8
Twintersh <tom.wintersheim@gmail.com>
2024-12-04 12:21:05 +0100

    Added moveline in splitData.py

--- splitDataset.py
+++ splitDataset.py
@@ -11,6 +11,16 @@ def linecount(file_path):
 	with open(file_path, 'r') as f:
 		return sum(1 for _ in f)
 
+def moveLines(extract, load, nb):
+	to_mv = linecount(extract) - nb
+	with open(extract, 'r') as ext:
+		lines = ext.readlines()
+	lines_to_mv = lines[:to_mv]
+	lines_to_keep = lines[to_mv:]
+	with open(extract, 'w') as ext, open(load, 'a') as ld:
+		ext.writelines(lines_to_keep)
+		ld.writelines(lines_to_mv)
+
 def main():
 	a = 0.50 # default split
 	datasets_dir = "datasets"
@@ -23,6 +33,7 @@ def main():
 	source_file		= "data.csv"
 	train_file		= os.path.join(datasets_dir, "train.csv")
 	predict_file	= os.path.join(datasets_dir, "predict.csv")
+	waited_len		= int(linecount(source_file) * a)
 
 	with open(source_file, 'r') as data, open(train_file, 'w') as train, open(predict_file, 'w') as predict:
 		for line in data:
@@ -30,6 +41,10 @@ def main():
 				train.write(line)
 			else:
 				predict.write(line)
+	if linecount(train_file) > waited_len:
+		moveLines(train_file, predict_file, waited_len)
+	elif linecount(train_file) < waited_len:
+		moveLines(predict_file, train_file, linecount(source_file) - waited_len)
 
 	print(f"length train file:\t{linecount(train_file)}")
 	print(f"length predict file:\t{linecount(predict_file)}")
